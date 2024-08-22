271077b6df9bb13810e225bb0472d8dae22d3c6e
Pratiksha B. Gaikwad <pratiksha4399@gmail.com>
2024-08-22 17:31:58 -0400

    append gemCC wfns

--- pyci/fanci/__init__.py
+++ pyci/fanci/__init__.py
@@ -10,6 +10,9 @@ __all__ = [
     "AP1roG",
     "DetRatio",
     "pCCDS",
+    "AP1roGSDSpin",
+    "AP1roGSDGeneralized",
+    "APG1roSD",
 ]
 
 
@@ -18,3 +21,6 @@ from .apig import APIG
 from .ap1rog import AP1roG
 from .detratio import DetRatio
 from .pccds import pCCDS
+from .ap1rogsdspin import AP1roGSDSpin
+from .ap1rogsdgeneralized import AP1roGSDGeneralized
+from .apg1rosd import APG1roSD
